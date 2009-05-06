7604f01ff177cd3413dee858b375694c07cf7e2b
Farzad FARID <ffarid@pragmatic-source.com>
2009-05-06 08:51:18 +0200

    New test on mtOrganization and code refactoring.
    * Add test on mtOrganization
    * Add LDAP modification help
    * Rework tests to use temporary files instead of env variables

--- tests/ldap-test.sh
+++ tests/ldap-test.sh
@@ -29,12 +29,15 @@
 # Consider unset variables as errors
 set -u
 
+TEST_DIR=$(dirname $0)
+export TEST_DIR
+
 # Note that we use a pre-generated LDIF file as the LDAP DIT during
 # tests, it is called "ldap-test.ldif".
 # Any important DIT modification must be reproduced in this file
 # during the OpenLDAP integration.
 oneTimeSetUp() {
-  source ldap-test.conf
+  source ${TEST_DIR}/ldap-test.conf
 
   # XXX We need access to the shunit temporary directory with a
   # non-root user. This is kind of hacky.
@@ -52,6 +55,10 @@ oneTimeSetUp() {
   chown openldap:openldap $LDAP_RUNDIR $LDAP_DBDIR
   chmod 755 $LDAP_RUNDIR $LDAP_DBDIR
   # The test LDAP server will be started by the first test
+
+  # Some temporary filenames
+  TMPFILE1=${shunit_tmpDir}/tmpfile1
+  TMPFILE2=${shunit_tmpDir}/tmpfile2
 }
 
 oneTimeTearDown() {
@@ -59,6 +66,26 @@ oneTimeTearDown() {
   kill $(cat ${LDAP_RUNDIR}/slapd.pid)
   # The LDAP directories we set up previously are automatically destroyed by shunit2
 }
+
+# Clean temp files between tests
+tearDown() {
+  rm -f ${TMPFILE1} ${TMPFILE2}
+}
+
+# --------------------------------
+# Helper functions
+# --------------------------------
+
+
+ldapsearch_anon() { ${LDAPSEARCH} -x -H "${LDAP_URI}" -b "${LDAP_BASE}" $@ ; }
+ldapsearch_admin() { ${LDAPSEARCH} -x -H "${LDAP_URI}" -b "${LDAP_BASE}" -D "${LDAP_ADMIN_DN}" -w "${LDAP_ADMIN_PW}" $@ ; }
+ldapmodify_admin() { ${LDAPMODIFY} -x -H "${LDAP_URI}" -D "${LDAP_ADMIN_DN}" -w "${LDAP_ADMIN_PW}" $@ ; }
+
+
+# ---------------------------------
+# Tests
+# ---------------------------------
+
 
 # We really load and start the slapd server in the first test
 testLoadAndStartSlapd() {
@@ -70,18 +97,75 @@ testLoadAndStartSlapd() {
   ${SLAPD} -n "ldap-test" -h "${LDAPN_URI} ${LDAPS_URI} ldapi:///" -g openldap -u openldap -f ${LDAP_CONFDIR}/slapd.conf
   RC=$?
   assertTrue "Failed to start slapd server" $RC || startSkipping
-  sleep 2
+  sleep 1
   assertTrue "Failed to start slapd server" "[ -f ${LDAP_RUNDIR}/slapd.pid ]"
 }
 
 testSSLConnection() {
   local OUTPUT RC
 
-  OUTPUT=`echo "" | openssl s_client -CApath /etc/ssl/certs -connect ${LDAPS_HOST}:${LDAPS_PORT} 2>/dev/null`
-  echo $OUTPUT | grep -q "Verify return code: 0 (ok)"
+  echo "" | openssl s_client -CApath /etc/ssl/certs -connect ${LDAPS_HOST}:${LDAPS_PORT} > ${TMPFILE1} 2>/dev/null
+  grep -q "Verify return code: 0 (ok)" ${TMPFILE1}
   RC=$?
   assertTrue "Cannot connect to LDAP/SSL or certificate chain invalid" $RC
 }
 
+testFindCompany() {
+  local OUTPUT RC
+  
+  ldapsearch_admin -LLL '(&(objectClass=mtOrganization)(o=TBWA))' > ${TMPFILE1}
+
+  grep -qE '^dn: o=TBWA,ou=Customers,dc=mediatech,dc=fr$' ${TMPFILE1}
+  RC=$?
+  assertTrue "Cannot find TBWA customer" $RC
+
+  grep -qE '^mtStatus: online$' ${TMPFILE1}
+  RC=$?
+  assertTrue "Cannot find customer attribute 'mtStatus'" $RC
+}
+
+testFindMediatech() {
+  local OUTPUT GREPOUT RC
+
+  ldapsearch_admin -b "ou=Internal,$LDAP_BASE" -LLL '(objectClass=mtOrganization)' > ${TMPFILE1}
+
+  GREPOUT=$(grep -E '^dn:' ${TMPFILE1} | wc -l)
+  assertEquals "There should be only one company under ou=Internal" 1 "${GREPOUT}" || return
+
+  GREPOUT=$(grep -E '^o:' ${TMPFILE1})
+  assertEquals "Cannot find Mediatech object" "o: Mediatech" "${GREPOUT}" 
+}
+
+testModifyCompany() {
+  local OUTPUT GREPOUT RC
+
+  ldapsearch_admin -LLL '(&(objectClass=mtOrganization)(o=TBWA)(mtStatus=online))' > ${TMPFILE1}
+
+  GREPOUT=$(grep -E '^dn:' ${TMPFILE1} | wc -l)
+  assertEquals "Cannot find TBWA customer with online status" 1 "${GREPOUT}" || return
+
+  cat <<EOT > ${TMPFILE1}
+dn: o=TBWA,ou=Customers,dc=mediatech,dc=fr
+changetype: modify
+replace: mtStatus
+mtStatus: offline
+EOT
+  ldapmodify_admin -f ${TMPFILE1}
+  RC=$?
+  assertTrue "LDAP modification failed" $RC || return
+
+  ldapsearch_admin -LLL '(&(objectClass=mtOrganization)(o=TBWA)(mtStatus=offline))' > ${TMPFILE1}
+
+  GREPOUT=$(grep -E '^dn:' ${TMPFILE1} | wc -l)
+  assertEquals "Cannot find TBWA customer with offline status" 1 "${GREPOUT}"
+}
+
+testFindMultipleCompanies() {
+  local OUTPUT GREPOUT RC
+
+  ldapsearch_admin -LLL '(&(objectClass=mtOrganization)(o=Autoworld))' > ${TMPFILE1}
+}
+
+
 # Now launch the test suite
-. shunit2
+. ${TEST_DIR}/shunit2
